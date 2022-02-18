8cc517691ab08859012dc97445db4288b377aa3e
noe <noe@marleix.com>
2022-02-18 11:42:06 +0100

    all endpoints created and methods (empty) created

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-var corsOptions = {
-    origin: "http://localhost:3000"
-};
 
-app.use(cors(corsOptions));
-// parse requests of content-type - application/json
 app.use(express.json());
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+
 // simple route
 app.get("/", (req, res) => {
     res.json({ message: "First endPoint" });
 });
 require("./routes/endPoint.js")(app);
-
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
 require("./routes/endPoint.js")(app);
