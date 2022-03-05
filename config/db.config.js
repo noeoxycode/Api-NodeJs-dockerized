34099f1b5b491cd6d73609659fc6989519543bbb
noe <noe@marleix.com>
2022-03-06 00:59:39 +0100

    bug fixed connexion db ok

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "welcome123",
+    HOST: "mysql",
+    USER: "nono",
+    PASSWORD: "nono",
     DB: "library"
 };
--- models/db.js
+++ models/db.js
@@ -2,15 +2,16 @@ const mysql = require("mysql");
 const dbConfig = require("../config/db.config.js");
 
 const connection = mysql.createConnection({
-    host: dbConfig.HOST,
-    user: dbConfig.USER,
-    password: dbConfig.PASSWORD,
-    database: dbConfig.DB,
+    host: "mysql",
+    user: "nono",
+    password: "nono",
+    database: "library",
     //can connect from client that is not human
     insecureAuth: true
 });
 // open the MySQL connection
 connection.connect(error => {
+    console.log(dbConfig);
     if (error) throw error;
     console.log("Connected to the database.");
 });
